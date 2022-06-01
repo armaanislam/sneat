4a7a283c8349d834385a45e5630bb5fbc17c734c
Arman Islam <armaanislam28@gmail.com>
2022-06-01 06:07:03 +0600

    validate username and email

--- base/views.py
+++ base/views.py
@@ -4,9 +4,12 @@ from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-#from django.contrib.auth.models import User
-from .models import User
+from django.db import IntegrityError
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from .forms import UserForm, MyUserCreationForm
+from .models import User
+
 
 @login_required(login_url='auth-login-basic')
 def indexPage(request):
@@ -53,16 +56,25 @@ def registerPage(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
+        try:
+            validate_email(email)
+            password = request.POST['password']
+            user = User.objects.filter(Q(username=username) | Q(email=email))
+            if user.exists():
+                messages.error(request, 'Username or email already exists')
+            else:
+                user = User.objects.create(
+                    username=username,
+                    email=email,
+                    password=password
+                )
+                user.save()
+                messages.success(request, 'Account created successfully!')
+                login(request, user)
+                return redirect('index')
+        except ValidationError:
+            messages.error(request, 'Enter a valid email')
         password = request.POST['password']
-        user = User.objects.create(
-            username=username,
-            email=email,
-            password=password
-        )
-        user.save()
-        messages.success(request, '')
-        login(request, user)
-        return redirect('index')
     else:
         messages.error(request, '')
     context = {}
