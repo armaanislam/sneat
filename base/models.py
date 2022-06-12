84c48cf33011a81b83d57db01d7d4745fef914bf
MD. Arman Islam <armaanislam84@gmail.com>
2022-06-12 15:38:12 +0600

    Before edit and delete config

--- base/models.py
+++ base/models.py
@@ -72,6 +72,10 @@ class KPIConfig(models.Model):
 
 class Supervisor(models.Model):
     name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='supervisor_created_by')
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='supervisor_updated_by')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
